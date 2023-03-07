8c8f3c3ff99edba9f6e56d554d8c5d1d0c2f679b
AnastasiiaDiab <lyaastud@gmail.com>
2023-03-07 13:32:43 -0500

    US10 feature, stepDefs, CalendarEventPage is added

---
+++ src/test/java/com/perfleet/pages/CalendarEventsPage_AD.java
@@ -0,0 +1,23 @@
+package com.perfleet.pages;
+
+import com.perfleet.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CalendarEventsPage_AD extends BasePage{
+
+    public CalendarEventsPage_AD(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(xpath = "//span[.='Calendar Events']")
+    public WebElement calendarEventModule;
+    @FindBy(xpath = "(//div[@class='btn-group'])[1]/a")
+    public WebElement createCalendarEventBtn;
+    @FindBy(tagName = "iframe")
+    public WebElement iframe;
+    @FindBy(id = "tinymce")
+    public WebElement descriptionBox;
+    @FindBy(tagName = "p")
+    public WebElement textInDescriptionBox;
+}
